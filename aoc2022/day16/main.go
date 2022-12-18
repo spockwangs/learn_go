c631e114a4ca764158744a4cd73b3929172d7110
spockwang <wbbtiger@gmail.com>
2022-12-18 16:37:00 +0800

    fix

--- aoc2022/day16/main.go
+++ aoc2022/day16/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/learn_go/aoc2022/util"
 	"os"
 	"strings"
+	"sort"
 )
 
 func main() {
@@ -24,6 +25,7 @@ func main() {
 		valves.parseValve(line)
 	}
 	fmt.Printf("%v\n", valves.startFrom("AA", 30))
+	fmt.Printf("%v\n", valves.startFrom2("AA", 26))
 }
 
 type Valves struct {
@@ -82,23 +84,29 @@ func (this *Valves) parseValve(s string) {
 	this.graph[valve.name] = valve
 }
 
-func (this Valves) startFrom(start string, timeLimit int) int {
+func (this *Valves) startFrom(start string, timeLimit int) int {
 	leftValves := []string{}
 	for k, v := range this.graph {
 		if v.flowRate > 0 {
 			leftValves = append(leftValves, k)
 		}
 	}
-	return this.startFromHelper(start, timeLimit, leftValves)
+	return this.startFromHelper(start, timeLimit, leftValves, nil)
 }
 
-func (this Valves) startFromHelper(start string, leftTime int, leftValves []string) int {
+func (this *Valves) startFromHelper(
+	start string, leftTime int, leftValves []string, ctx *Context) int {
 	bestScore := 0
 	for i, next := range leftValves {
 		dist := this.computeDistance(start, next)
 		if leftTime > dist + 1 {
-			score := (leftTime - dist - 1) * this.graph[next].flowRate +
-				this.startFromHelper(next, leftTime-dist-1, remove(leftValves, i))
+			curScore := (leftTime - dist - 1) * this.graph[next].flowRate
+			var newCtx *Context = nil
+			if ctx != nil {
+				newCtx = ctx.appendPathScore(next, curScore)
+			}
+			score := curScore +
+				this.startFromHelper(next, leftTime-dist-1, remove(leftValves, i), newCtx)
 			if score > bestScore {
 				bestScore = score
 			}
@@ -114,18 +122,24 @@ func (this Valves) startFrom2(start string, timeLimit int) int {
 			leftValves = append(leftValves, k)
 		}
 	}
-	return this.startFromHelper2(start, timeLimit, leftValves)
-}
 
-func (this Valves) startFromHelper2(start string, leftTime int, leftValves []string) int {
-	bestScore := 0
-	for i, next := range leftValves {
-		dist := this.computeDistance(start, next)
-		if leftTime > dist + 1 {
-			score := (leftTime - dist - 1) * this.graph[next].flowRate +
-				this.startFromHelper(next, leftTime-dist-1, remove(leftValves, i))
-			if score > bestScore {
-				bestScore = score
+	ctx := newContext()
+	this.startFromHelper(start, timeLimit, leftValves, ctx)
+
+	pathScores := []PathScore{}
+	for _, v := range ctx.pathScoreMap {
+		pathScores = append(pathScores, v)
+	}
+	bestScore := 0 
+	for i := range pathScores {
+		for j := i+1; j < len(pathScores); j++ {
+			path1 := pathScores[i].path
+			path2 := pathScores[j].path
+			if !intersect(path1, path2) {
+				score := pathScores[i].score + pathScores[j].score
+				if bestScore < score {
+					bestScore = score
+				}
 			}
 		}
 	}
@@ -133,29 +147,25 @@ func (this Valves) startFromHelper2(start string, leftTime int, leftValves []str
 }
 
 // Compute the shortest distance using BFS.
-func (this Valves) computeDistance(src, dest string) int {
+func (this *Valves) computeDistance(src, dest string) int {
 	d, ok := this.dist[makePair(src, dest)]
 	if ok {
 		return d
 	}
 	
+	visited := make(map[string]bool)
+	visited[src] = true
 	tentativeNodes := make(map[string]bool)
 	tentativeNodes[src] = true
 	depth := 0
-	visited := make(map[string]bool)
 	for len(tentativeNodes) > 0 {
 		nextLevel := make(map[string]bool)
 		for cur := range tentativeNodes {
-			if src < cur {
-				this.dist[makePair(src, cur)] = depth
-			} else {
-				this.dist[makePair(src, cur)] = depth
-			}
+			this.dist[makePair(src, cur)] = depth
 			if cur == dest {
 				return depth
 			}
 
-			visited[cur] = true
 			neighbours := this.graph[cur].neighbours
 			for _, n := range neighbours {
 				_, ok := visited[n]
@@ -163,6 +173,7 @@ func (this Valves) computeDistance(src, dest string) int {
 					continue
 				}
 				nextLevel[n] = true
+				visited[n] = true
 			}
 		}
 		tentativeNodes = nextLevel
@@ -177,4 +188,61 @@ func remove(s []string, idx int) []string {
 	copy(ret, s)
 	ret[idx] = ret[len(ret)-1]
 	return ret[:len(ret)-1]
+}
+
+type Context struct {
+	pathScoreMap map[string]PathScore
+	path []string
+	score int
+}
+
+type PathScore struct {
+	path []string
+	score int
+}
+
+func newContext() *Context {
+	return &Context{
+		pathScoreMap: make(map[string]PathScore),
+	}
+}
+
+func (this *Context) appendPathScore(node string, score int) *Context {
+	ret := &Context{
+		pathScoreMap: this.pathScoreMap,
+		path: make([]string, len(this.path)),
+		score: this.score,
+	}
+	copy(ret.path, this.path)
+	ret.path = append(ret.path, node)
+	ret.score += score
+	
+	path := make([]string, len(ret.path))
+	copy(path, ret.path)
+	sort.Slice(path, func(i, j int) bool {
+		return path[i] < path[j]
+	})
+	pathStr := strings.Join(path, "-")
+	pathScore, ok := this.pathScoreMap[pathStr]
+	if !ok || pathScore.score < ret.score {
+		this.pathScoreMap[pathStr] = PathScore{
+			path: ret.path,
+			score: ret.score,
+		}
+	}
+	return ret
+}
+
+func intersect(p1, p2 []string) bool {
+	set := make(map[string]bool)
+	for _, n := range p1 {
+		set[n] = true
+	}
+	for _, n := range p2 {
+		_, ok := set[n]
+		if ok {
+			return true
+		}
+	}
+	return false
 }
